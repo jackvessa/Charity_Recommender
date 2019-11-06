4cc2ef9fef60cc511993bb73b8c1944eb3b1a4c8
Jack Vessa <jack.k.vessa@vanderbilt.edu>
2019-11-05 18:24:57 -0800

    Adding Images

---
+++ IMG/Charity_Recommender_Functions.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import datetime
+import math
+from uszipcode import SearchEngine
+
+
+def find_zip_state(df,zipcode):
+    '''
+    Function
+    --------
+    Looks through DataFrame and returns Entries with same state
+
+    Parameters
+    ----------
+    df : Pandas DataFrame of Charities
+    zip_code : Zip Code to Match on
+
+    Returns
+    -------
+    Pandas DataFrame with Entries matching provided state
+    '''
+    search = SearchEngine(simple_zipcode=True)
+    state = search.by_zipcode(zipcode).state
+    temp_df = df[df['STATE']==state]
+
+    return temp_df
+
+
+def search_zip_to_state_major_category(df, zipcode, category):
+    '''
+    Function
+    --------
+    Filter a Pandas DataFrame by Zip Code and NTEE Major Category
+
+    Parameters
+    ----------
+    df : Pandas DataFrame with 'NTEE_Major_Category' column
+    zipcode : Zipcode to Filter on
+    category : NTEE Major Category to Filter on
+
+    Return
+    ------
+    Filtered Pandas DataFrame
+    '''
+    temp_df = find_zip_state(df,zipcode)
+    temp_df = find_major_category(temp_df,category)
+
+    return temp_df
+
+def add_score_and_sort_df(df,zipcode,zip_factor = 20,county_factor = 10,state_factor=1,top_char=3):
+    '''
+    Function
+    --------
+    ADD score column to Pandas DataFrame and return top X rated Charities by score
+
+    Parameters
+    ----------
+    df : Pandas DataFrame with 'ZIP_FIVE' column
+    zipcode : Zipcode to Filter on
+    zip_factor : Weighting Variable for Charities with Same Zip Code
+    county_factor : Weighting Variable for Charities with Same Zip Code
+    state_factor : Weighting Variable for Charities with Same State
+    top_char : Number of "Top" results to return sorted by score descending
+
+    Return
+    ------
+    Filtered Pandas DataFrame
+    '''
+    county = search.by_zipcode(zipcode).county
+    state = search.by_zipcode(zipcode).state
+
+    df['zip_factor'] = df['ZIP_FIVE'].apply(lambda x: zip_factor if int(x) == zipcode else 0)
+    df['county_factor'] = df['County'].apply(lambda x: county_factor if x == county else 0)
+    df['state_factor'] = df['STATE'].apply(lambda x: state_factor if x == state else 0)
+
+    df['score'] = df['INCOME_AMT']*(1+df['zip_factor']+df['county_factor']+df['state_factor'])
+
+    temp_df = df.sort_values(by='score',ascending=False).head(top_char)
+
+    return temp_df
